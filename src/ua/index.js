7fed30a29ce87cb9434dd07f47b761762996e251
yang24201 <yang24201@126.com>
2017-12-22 14:02:03 +0800

    change sdk name

--- src/ua/index.js
+++ src/ua/index.js
@@ -3,4 +3,4 @@
 var version = require('../version');
 var comments = [process.env.CLIENT_PLATFORM || 'Node.js'].concat(require('./comments'));
 
-module.exports = 'LeanCloud-JS-SDK/' + version + ' (' + comments.join('; ') + ')';
+module.exports = 'Lvyii-JS-SDK/' + version + ' (' + comments.join('; ') + ')';
